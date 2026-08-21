f72f5606f019b85db9776e77b6987d006691b017
jeremytheva <jeremytheva@gmail.com>
2026-08-21 22:42:25 +1000

    test: import URL explicitly in route splitting contract

--- test/route-code-splitting.test.mjs
+++ test/route-code-splitting.test.mjs
@@ -1,6 +1,7 @@
 import test from 'node:test'
 import assert from 'node:assert/strict'
 import fs from 'node:fs/promises'
+import { URL } from 'node:url'
 
 const read = (path) => fs.readFile(new URL(`../${path}`, import.meta.url), 'utf8')
 
